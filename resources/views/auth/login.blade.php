a39542343961ceeb8794e68e88d88e14a5e98e20
Aldhi Xar <aldhi.x@gmail.com>
2023-03-22 20:31:26 +0700

    comment

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,45 @@
 @extends('layouts.login')
 @section('content')
 <div class="login-logo">
-    <a href="/"><b>Login</b>{{ config('app.name') }}</a>
-  </div>
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
-      <form action="?">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email" />
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
+  <a href="/"><b>Login</b>{{ config('app.name') }}</a>
+</div>
+<div class="card">
+  <div class="card-body login-card-body">
+    <p class="login-box-msg">Sign in to start your session</p>
+    <form action="?">
+      <div class="input-group mb-3">
+        <input type="email" class="form-control" placeholder="Email" />
+        <div class="input-group-append">
+          <div class="input-group-text">
+            <span class="fas fa-envelope"></span>
           </div>
         </div>
-        <div class="input-group mb-3">
-          <input
-            type="password"
-            class="form-control"
-            placeholder="Password"
-          />
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
+      </div>
+      <div class="input-group mb-3">
+        <input type="password" class="form-control" placeholder="Password" />
+        <div class="input-group-append">
+          <div class="input-group-text">
+            <span class="fas fa-lock"></span>
           </div>
         </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember" />
-              <label for="remember"> Remember Me </label>
-            </div>
+      </div>
+      <div class="row">
+        <div class="col-8">
+          <div class="icheck-primary">
+            <input type="checkbox" id="remember" />
+            <label for="remember"> Remember Me </label>
           </div>
+        </div>
 
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">
-              Sign In
-            </button>
-          </div>
+        <div class="col-4">
+          <button type="submit" class="btn btn-primary btn-block">
+            Sign In
+          </button>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   </div>
+</div>
 @endsection
 @push('css-plugins')
 <link rel="stylesheet" href="{{asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
--- resources/views/layouts/inc/admin/navbar.blade.php
+++ resources/views/layouts/inc/admin/navbar.blade.php
@@ -3,8 +3,9 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
+        <!-- Nav Item -->
     </ul>
-
     <ul class="navbar-nav ml-auto">
+        <!-- Nav Item -->
     </ul>
 </nav>
