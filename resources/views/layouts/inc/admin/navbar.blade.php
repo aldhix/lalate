345a9b9820b4f6a3e49714346e078f9b8d037e98
Aldhi Xar <aldhi.x@gmail.com>
2023-03-28 04:14:31 +0700

    navbar

--- resources/views/layouts/inc/admin/navbar.blade.php
+++ resources/views/layouts/inc/admin/navbar.blade.php
@@ -11,18 +11,24 @@
                 Fullname
                 <i class="fas fa-caret-down"></i>
             </a>
-            <div class="dropdown-menu dropdown-menu-right text-sm">
+            <div class="dropdown-menu dropdown-menu-right">
                 <a href="#" class="dropdown-item">
                     <i class="fas fa-user-circle mr-2"></i> My Profile
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#" onclick="event.preventDefault();document.getElementById('form-logout').submit()" class="dropdown-item">
+                <a href="#" onclick="event.preventDefault();document.getElementById('form-logout').submit()"
+                    class="dropdown-item">
                     <i class="fas fa-sign-out-alt mr-2"></i> Log Out
                 </a>
                 <form id="form-logout" method="post" action="#">
                     @csrf
                 </form>
             </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </a>
         </li>
     </ul>
 </nav>
